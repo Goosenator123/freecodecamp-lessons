6236137670817b9eb01ad47496fc7ae09de10643
Simon Cao <simon.cao2007@gmail.com>
2023-12-23 23:50:54 -0500

    Created switchStatment.js as an introduction to Switch statments\n\nDeleted arrays.js and booleans.js and added their respective code into dataTypes.js\n\nDeleted braketNotationForStrings.js and added its code into braketNotations.js\n\nDeleted equalityOperators.js and shorthandAssigmentOperators.js and added their respective code in operators.js

--- JavaScript/BasicJavaScript/dataTypes.js
+++ JavaScript/BasicJavaScript/dataTypes.js
@@ -3,6 +3,11 @@
 //! ==============================
 // JavaScript is a dynamically typed language, meaning that variables can hold values of different types. This script introduces some of the fundamental data types in JavaScript and demonstrates their usage.
 
+console.log(`
+==========
+Data Types
+==========`)
+
 // 1. Numbers
 let myAge = 25;
 let price = 19.99;
@@ -29,6 +34,7 @@ let person = {
   firstName: 'Alice',
   lastName: 'Smith',
   age: 30,
+  pets: ["Dog", "Cats"]
 };
 
 // Displaying Data Types
@@ -40,3 +46,219 @@ console.log("4. Undefined:", undefinedVariable);
 console.log("5. Null:", nullValue);
 console.log("6. Arrays:", fruits);
 console.log("7. Objects:", person);
+
+
+//! =========================================
+//! ||  JavaScript - Arrays and Nested Arrays  ||
+//! =========================================
+// Arrays are versatile data structures in JavaScript that allow you to store and organize multiple values within a single variable. They are used to create lists of elements, where each element can be of any data type, including numbers, strings, or even other arrays. This script introduces the concept of arrays and explores their various features, including creating, accessing, modifying, and working with nested arrays.
+
+console.log(`
+==========
+Arrays and Nested Arrays
+==========`)
+
+// Introduction to Arrays
+console.log("- Introduction to Arrays:");
+
+// Creating an array
+let fruitList = ["apple", "banana", "orange", "grape"];
+
+// Accessing elements using index
+console.log("Array:", fruitList);
+console.log("Element at index 1:", fruitList[1]);
+console.log("Last element:", fruitList[fruitList.length -1]);
+
+// Introduction to Nested Arrays
+console.log("\n- Introduction to Nested Arrays:");
+
+// Creating a nested array
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+// Accessing elements in a nested array
+console.log("Nested Array:", matrix);
+console.log("Element at row 1, column 2:", matrix[1][2]);
+console.log("Last element of the last row and the last column:", matrix[matrix.length -1][matrix[matrix.length -1].length -1]);
+
+
+//! ==============================
+//! ||  JavaScript - Booleans  ||
+//! ==============================
+// Booleans are a fundamental data type in JavaScript that represent logical values - true or false. Booleans are often used in programming to make decisions and control the flow of a program based on certain conditions.
+
+console.log(`
+==========
+Booleans
+==========`)
+
+// Example 1: Simple Boolean Variables
+let isSunny = true;
+let isRaining = false;
+
+console.log("Example 1:");
+console.log("Is it sunny today?", isSunny); // Output: true
+console.log("Is it raining now?", isRaining); // Output: false
+
+// Example 2: Boolean Expressions
+let temperature = 25;
+let isWarm = temperature > 20; // Evaluates to true if temperature is greater than 20
+
+console.log("\nExample 2:");
+console.log("Is it warm?", isWarm); // Output: true
+
+// Example 3: Using Booleans in Conditional Statements
+let hasMoney = true;
+
+console.log("\nExample 3:");
+if (hasMoney) {
+  console.log("I can buy something!");
+} else {
+  console.log("I need to save money.");
+}
+
+// Example 4: Boolean Operations
+let hasCoffee = true;
+let hasMilk = false;
+
+console.log("\nExample 4:");
+let canMakeCoffee = hasCoffee && hasMilk; // Using logical AND
+console.log("Can I make coffee?", canMakeCoffee); // Output: false
+
+let shouldTakeUmbrella = isRaining || isSunny; // Using logical OR
+console.log("Should I take an umbrella?", shouldTakeUmbrella); // Output: true
+
+let isCold = !isWarm; // Using logical NOT
+console.log("Is it cold?", isCold); // Output: false
+
+
+//! ==============================
+//! ||  JavaScript Objects  ||
+//! ==============================
+// Objects in JavaScript are powerful data structures that allow you to store and organize related information. They consist of key-value pairs, where each key is a unique identifier for a value. This script provides an introduction to JavaScript objects, covering their creation, properties, methods, and common operations. Understanding objects is crucial for working with complex data and building dynamic applications.
+
+console.log(`
+==========
+JavaScript Objects
+==========`)
+
+// Creating an Object
+console.log("- Creating an Object:");
+
+// Object literal notation
+let person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isStudent: false,
+};
+
+// Accessing Object Properties (with dot notation or bracket notation)
+console.log("\n- Accessing Object Properties:");
+
+console.log("First Name:", person1.firstName);
+console.log("Last Name:", person1["lastName"]);
+console.log("Age:", person1.age);
+console.log("Is Student:", person1.isStudent);
+
+// Modifying Object Properties
+console.log("\n- Modifying Object Properties:");
+
+person1.age = 31;
+person1["isStudent"] = true;
+
+console.log("Modified Age:", person1.age);
+console.log("Modified Is Student:", person1["isStudent"]);
+
+// Adding and Deleting Object Properties
+console.log("\n- Adding and Deleting Object Properties:");
+
+person1.city = "New York";
+delete person1.isStudent;
+
+console.log("Added City:", person1.city);
+console.log("Deleted Is Student:", person1.isStudent); // undefined
+
+// Object Methods
+console.log("\n- Object Methods:");
+
+let car = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2022,
+  startEngine: function () {
+    console.log("Engine started!");
+  },
+  stopEngine: function () {
+    console.log("Engine stopped.");
+  },
+};
+
+car.startEngine();
+car.stopEngine();
+
+
+//! =======================================
+//! ||  JavaScript Nested Objects  ||
+//! =======================================
+// Nested objects in JavaScript provide a powerful way to structure and organize data hierarchically. This concept allows you to represent complex relationships and encapsulate related information within a parent object. This script introduces nested objects, explaining their syntax, creation, access, and common operations. Understanding nested objects is crucial for managing multidimensional data structures and building more sophisticated applications.
+
+console.log(`
+==========
+JavaScript Nested Objects
+==========`)
+
+// Creating an Object with Nested Objects
+console.log("- Creating an Object with Nested Objects:");
+
+let person2 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    zipCode: "12345",
+  },
+  contacts: {
+    email: "john@example.com",
+    phone: "555-1234",
+  },
+};
+
+// Accessing Nested Object Properties (with dot notation or bracket notation)
+console.log("\n- Accessing Nested Object Properties:");
+console.log("First Name:", person2.firstName);
+console.log("Street Address:", person2.address.street);
+console.log("Email:", person2["contacts"]["phone"]);
+
+// Modifying Nested Object Properties
+console.log("\n- Modifying Nested Object Properties:");
+
+person2.address.city = "New City";
+person2["contacts"]["phone"] = "555-5678";
+
+console.log("Modified City:", person2.address.city);
+console.log("Modified Phone:", person2["contacts"]["phone"]);
+
+// Adding Nested Objects
+console.log("\n- Adding Nested Objects:");
+
+person2.education = {
+  degree: "Bachelor's",
+  school: "University XYZ",
+};
+
+console.log("Added Education Object:", person2.education);
+
+// Deleting Nested Object Properties
+console.log("\n- Deleting Nested Object Properties:");
+
+delete person2["contacts"]["phone"];
+delete person2.education;
+
+console.log("Deleted Phone:", person2["contacts"]["phone"]); // undefined
+console.log("Deleted Education Object:", person2.education); // undefined
--- JavaScript/BasicJavaScript/operators.js
+++ JavaScript/BasicJavaScript/operators.js
@@ -3,6 +3,11 @@
 //! ====================================
 // In JavaScript, operators are symbols that perform operations on operands. They play a crucial role in performing tasks like arithmetic calculations, string manipulations, comparisons, and logical evaluations. This script provides an introduction to some of the fundamental operators in JavaScript and illustrates their usage through examples.
 
+console.log(`
+==========
+Operators
+==========`)
+
 // Arithmetic Operators
 let num1 = 10;
 let num2 = 5;
@@ -27,22 +32,6 @@ let fullName = firstName + " " + lastName;
 console.log("\n- String Concatenation:");
 console.log("Full Name:", fullName);
 
-// Comparison Operators
-let isEqual = num1 === num2;
-let isNotEqual = num1 !== num2;
-let greaterThan = num1 > num2;
-let lessThan = num1 < num2;
-let greaterOrEqual = num1 >= num2;
-let lessOrEqual = num1 <= num2;
-
-console.log("\n- Comparison Operations:");
-console.log("Is Equal:", isEqual);
-console.log("Is Not Equal:", isNotEqual);
-console.log("Greater Than:", greaterThan);
-console.log("Less Than:", lessThan);
-console.log("Greater or Equal:", greaterOrEqual);
-console.log("Less or Equal:", lessOrEqual);
-
 // Logical Operators
 let andOperator = true && false;
 let orOperator = true || false;
@@ -52,3 +41,112 @@ console.log("\n- Logical Operations:");
 console.log("AND Operator:", andOperator);
 console.log("OR Operator:", orOperator);
 console.log("NOT Operator:", notOperator);
+
+
+//! ============================================
+//! ||  JavaScript - Comparison Operators  ||
+//! ============================================
+// Comparison operators in JavaScript are essential for evaluating conditions and making decisions in your code. They allow you to compare values, variables, or expressions, producing Boolean results (true or false). This script introduces various comparison operators, demonstrating their usage and explaining the outcomes based on the comparisons made.
+
+console.log(`
+==========
+Comparison Operators
+==========`)
+
+// Introduction to Equality Operators
+console.log("- Introduction to Equality Operators:");
+
+// Equal (==) operator
+console.log("5 == 5:", 5 == 5); // true
+console.log("5 == '5':", 5 == '5'); // true
+
+// Strict Equal (===) operator
+console.log("5 === 5:", 5 === 5); // true
+console.log("5 === '5':", 5 === '5'); // false
+
+// Not Equal (!=) operator
+console.log("5 != 10:", 5 != 10); // true
+
+// Strict Not Equal (!==) operator
+console.log("5 !== '5':", 5 !== '5'); // true
+
+// Introduction to Relational Operators
+console.log("\n- Introduction to Relational Operators:");
+
+// Greater Than (>) operator
+console.log("10 > 5:", 10 > 5); // true
+
+// Less Than (<) operator
+console.log("10 < 5:", 10 < 5); // false
+
+// Greater Than or Equal To (>=) operator
+console.log("10 >= 10:", 10 >= 10); // true
+
+// Less Than or Equal To (<=) operator
+console.log("10 <= 5:", 10 <= 5); // false
+
+
+//! ==============================
+//! ||  JavaScript Shorthand Assignment Operators with Different Types  ||
+//! ==============================
+// Shorthand assignment operators in JavaScript provide a concise way to perform operations and assignments. This script introduces various shorthand assignment operators for numbers, strings, arrays, and objects, showcasing their usage with different types.
+
+console.log(`
+==========
+Shorthand Assignemnt Operators
+==========`)
+
+// Number Shorthand Assignment Operators
+console.log("- Number Shorthand Assignment Operators");
+
+let numOne = 10;
+numOne += 5; // num1 = num1 + 5
+console.log("Number +=:", numOne);
+
+let numTwo = 20;
+numTwo -= 3; // num2 = num2 - 3
+console.log("Number -=:", numTwo);
+
+let numThree = 3;
+numThree *= 4; // num3 = num3 * 4
+console.log("Number *=:", numThree);
+
+let numFour = 15;
+numFour /= 3; // num4 = num4 / 3
+console.log("Number /=:", numFour);
+
+let numFive = 10;
+numFive %= 3; // num5 = num5 % 3
+console.log("Number %=:", numFive);
+
+// String Shorthand Assignment Operator
+console.log("\n- String Shorthand Assignment Operators");
+
+let strOne = "Hello";
+strOne += " World"; // str1 = str1 + " World"
+console.log("String +=:", strOne);
+
+// Array Shorthand Assignment Operators
+console.log("\n- Array Shorthand Assignment Operators");
+
+let arrOne = [1, 2, 3];
+arrOne.push(4); // Array push method for adding an element
+console.log("Array push (+=):", arrOne);
+
+let arrTwo = [1, 2, 3, 4, 5];
+arrTwo.pop(); // Array pop method for removing the last element
+console.log("Array pop (-=):", arrTwo);
+
+// Object Shorthand Assignment Operator (Merging)
+console.log("\n- Object Shorthand Assignment Operators (Merging)");
+
+let objOne = { a: 1, b: 2 };
+Object.assign(objOne, { c: 3 }); // Object.assign for merging
+console.log("Object.assign (+=):", objOne);
+
+// Object Shorthand Assignment Operator (Removal)
+console.log("\n- Object Shorthand Assignment Operators (Removal)");
+
+let objTwo = { a: 1, b: 2, c: 3 };
+delete objTwo.c; // Delete operator for property removal
+console.log("Delete operator (-=):", objTwo);
